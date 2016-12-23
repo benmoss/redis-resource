47f6ece4c287e480f80a339379db488f4886594f
Ben Moss <ben@mossity.com>
2016-12-23 15:31:04 -0500

    fix da ruby

--- scripts/is_prime.rb
+++ scripts/is_prime.rb
@@ -5,6 +5,7 @@ require 'prime'
 num = File.read("./numbers/value").chomp.to_i
 if Prime.prime?(num)
   puts "found prime #{num}!"
+  File.write("./output/stuff", num)
   exit 0
 else
   puts "{num} is not prime"
